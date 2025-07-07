9dcd7300fe14c880a030dd8b3ee8f732428aa1c5
letícia <157068692+Leticiaaa42@users.noreply.github.com>
2025-07-07 17:41:08 -0300

    Update CursoDao.java

--- BD_final-master/BD_matricula/src/model/dao/CursoDao.java
+++ BD_final-master/BD_matricula/src/model/dao/CursoDao.java
@@ -6,6 +6,7 @@ import model.entities.Curso;
 
 public interface CursoDao {
     void insert(Curso obj) throws Exception;
+    void massInsert(List<Curso> cursos) throws SQLException;
     void update(Curso obj) throws Exception;
     void deleteById(int id) throws Exception;
     Curso findById(int id) throws Exception;
