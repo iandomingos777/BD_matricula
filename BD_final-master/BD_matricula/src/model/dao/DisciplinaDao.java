226ecd78bc621c6912902f01326319833fc485d5
letícia <157068692+Leticiaaa42@users.noreply.github.com>
2025-07-07 17:41:23 -0300

    Update DisciplinaDao.java

--- BD_final-master/BD_matricula/src/model/dao/DisciplinaDao.java
+++ BD_final-master/BD_matricula/src/model/dao/DisciplinaDao.java
@@ -6,9 +6,12 @@ import model.entities.Disciplina;
 
 public interface DisciplinaDao {
     void insert(Disciplina obj) throws Exception;
+    void massInsert(List<Disciplina> disciplinas) throws SQLException;
     void update(Disciplina obj) throws Exception;
     void deleteById(int id) throws Exception;
     Disciplina findById(int id) throws Exception;
     List<Disciplina> findAll() throws Exception;
     List<Disciplina> findBySubstring(String sub) throws SQLException;
+
+    float mediaDisciplina(int id) throws SQLException;
 }
